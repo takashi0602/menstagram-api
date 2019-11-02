caed37a504f70b18c6e5e40f1a391100275e899b
Kazuki Tobita <kazukiti201@gmail.com>
2019-11-03 01:40:28 +0900

    Delete unnecessary code

--- database/seeds/CreateUserSeeder.php
+++ database/seeds/CreateUserSeeder.php
@@ -18,15 +18,6 @@ class CreateUserSeeder extends Seeder
     public function run()
     {
         User::truncate();
-
-//        User::create([
-//            'user_id'=> 'menstagram',
-//            'screen_name'=> 'menstagram',
-//            'email'=> 'menstagram@menstagram.com',
-//            'password'=> bcrypt('menstagram'),
-//
-//        ]);
-
         factory(User::class, 10)->create();
     }
 }
