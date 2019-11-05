4bfd76ff6904bd91dd7fb1d48c115e38bf47b2e8
Kazuki Tobita <kazukiti201@gmail.com>
2019-11-05 17:42:40 +0900

    Modify post api

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * 投稿系API
+ *
+ * Class PostController
+ * @package App\Http\Controllers\Api\V1
+ */
+class PostController extends Controller
+{
+    /**
+     * 投稿
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function post()
+    {
+        $response = [
+            'can_post'=> true,
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * 投稿に対するいいね
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function like()
+    {
+        return response([], 200);
+    }
+
+    /**
+     * 投稿に対するいいねを外す
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function unlike()
+    {
+        return response([], 200);
+    }
+
+    /**
+     * 投稿の詳細を見る
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function detail()
+    {
+        $response = [
+            [
+                'id'=> 1,
+                'text'=> 'ダミーテキスト',
+                'images'=> [
+                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F1',
+                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F2',
+                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F3',
+                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F4'
+                ],
+                'liked'=> 1,
+                'liker'=> [
+                    [
+                        'user_id'=> 'ダミーデータさん',
+                        'avatar'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                    ],
+                    [
+                        'user_id'=> 'ダミーデータさん',
+                        'avatar'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                    ]
+                ],
+                'created_at'=> 'ダミーデータさん',
+                'updated_at'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+            ]
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * 投稿にいいねしたユーザー一覧を見る
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function liker()
+    {
+        $response =  [
+            [
+                'id' =>  1,
+                'user'=> [
+                    'user_id'=> 'test_mock',
+                    'screen_name'=> 'ダミーデータさん',
+                    'avater'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                ]
+            ]
+        ];
+
+        return response($response, 200);
+    }
+}
