c450ab16814ba1e228d8f60705a1476194ca9cfd
Kazuki Tobita <kazukiti201@gmail.com>
2019-11-06 11:55:58 +0900

    Add /api/v1/post/media

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -20,7 +20,21 @@ class PostController extends Controller
     public function post()
     {
         $response = [
-            'can_post'=> true,
+            'can_posted' => true,
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * 画像送信
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function media()
+    {
+        $response = [
+            'post_id' => 1,
         ];
 
         return response($response, 200);
@@ -55,28 +69,28 @@ class PostController extends Controller
     {
         $response = [
             [
-                'id'=> 1,
-                'text'=> 'ダミーテキスト',
-                'images'=> [
+                'id' => 1,
+                'text' => 'ダミーテキスト',
+                'images' => [
                     'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F1',
                     'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F2',
                     'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F3',
-                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F4'
+                    'https://placehold.jp/500x500.png?text=%E7%94%BB%E5%83%8F4',
                 ],
-                'liked'=> 1,
-                'liker'=> [
+                'liked' => 1,
+                'liker' => [
                     [
-                        'user_id'=> 'ダミーデータさん',
-                        'avatar'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                        'user_id' => 'ダミーデータさん',
+                        'avatar' => 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3',
                     ],
                     [
-                        'user_id'=> 'ダミーデータさん',
-                        'avatar'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                        'user_id' => 'ダミーデータさん',
+                        'avatar' => 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3',
                     ]
                 ],
-                'created_at'=> 'ダミーデータさん',
-                'updated_at'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
-            ]
+                'created_at' => 'ダミーデータさん',
+                'updated_at' => 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3',
+            ],
         ];
 
         return response($response, 200);
@@ -92,10 +106,10 @@ class PostController extends Controller
         $response =  [
             [
                 'id' =>  1,
-                'user'=> [
-                    'user_id'=> 'test_mock',
-                    'screen_name'=> 'ダミーデータさん',
-                    'avater'=> 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3'
+                'user' => [
+                    'user_id' => 'test_mock',
+                    'screen_name' => 'ダミーデータさん',
+                    'avater' => 'https://placehold.jp/150x150.png?text=%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3',
                 ]
             ]
         ];
