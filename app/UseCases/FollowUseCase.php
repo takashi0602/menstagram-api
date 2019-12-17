f1eb055f7f0a099d7f23bdd54c3d669eea27f9e9
Kazuki Tobita <kazukiti201@gmail.com>
2019-12-18 05:21:29 +0900

    Add follow & unfollow api

---
+++ app/UseCases/FollowUseCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\UseCases;
+
+use App\Models\Follow;
+
+/**
+ * フォロー
+ *
+ * Class FollowUseCase
+ * @package App\UseCases
+ */
+class FollowUseCase
+{
+    /**
+     * @param $targetUserId
+     * @return bool
+     */
+    public function __invoke($targetUserId)
+    {
+        $id = user()->id;
+        $targetId = user($targetUserId)->id;
+
+        if (Follow::where('user_id', $id)->where('target_user_id', $targetId)->exists()) return false;
+
+        Follow::create([
+            'user_id'        => $id,
+            'target_user_id' => $targetId,
+        ]);
+
+        return true;
+    }
+}
