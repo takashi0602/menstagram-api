68796491b17cb5a8e625d42c0e69075a3f9660ee
Kazuki Tobita <kazukiti201@gmail.com>
2019-12-29 06:22:20 +0900

    Modify following test

--- tests/Feature/DataProviders/UserFollowingDataProvider.php
+++ tests/Feature/DataProviders/UserFollowingDataProvider.php
@@ -10,5 +10,51 @@ namespace Tests\Feature\DataProviders;
  */
 trait UserFollowingDataProvider
 {
-    //
+    /**
+     * 正常系(type)のテストデータの定義
+     *
+     * @return array
+     */
+    public function successTypeProvider()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * 異常系(ユーザーID)のテストデータの定義
+     *
+     * @return array
+     */
+    public function failUserIdProvider()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * 異常系(フォローID)のテストデータの定義
+     *
+     * @return array
+     */
+    public function failFollowIdProvider()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * 異常系(type)のテストデータの定義
+     *
+     * @return array
+     */
+    public function failTypeProvider()
+    {
+        return [
+
+        ];
+    }
 }
--- tests/Feature/UserFollowingTest.php
+++ tests/Feature/UserFollowingTest.php
@@ -15,7 +15,67 @@ class UserFollowingTest extends TestCase
 {
     use UserFollowingDataProvider;
 
+    /**
+     * 初期化処理
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        parent::seeding([\CreateUsersSeeder::class, \CreateFollowsSeeder::class]);
+    }
+
+    /**
+     * 正常系
+     */
     public function successCase()
+    {
+        //
+    }
+
+    /**
+     * 正常系(ユーザーIDの指定あり)
+     */
+    public function successUserIdCase()
+    {
+        //
+    }
+
+    /**
+     * 正常系(フォローIDの指定あり)
+     */
+    public function successFollowIdCase()
+    {
+        //
+    }
+
+    /**
+     * 正常系(フォローID, typeの指定あり)
+     */
+    public function successTypeCase()
+    {
+        //
+    }
+
+    /**
+     * 異常系(ユーザーID)
+     */
+    public function failUserIdCase()
+    {
+        //
+    }
+
+    /**
+     * 異常系(フォローID)
+     */
+    public function failFollowIdCase()
+    {
+        //
+    }
+
+    /**
+     * 異常系(type)
+     */
+    public function failTypeCase()
     {
         //
     }
