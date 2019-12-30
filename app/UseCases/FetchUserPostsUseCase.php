39e0db4362957d15187640207eda3c1aaa1bb966
Kazuki Tobita <kazukiti201@gmail.com>
2019-12-31 05:35:07 +0900

    Add user posts api

---
+++ app/UseCases/FetchUserPostsUseCase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\UseCases;
+
+/**
+ * ユーザーの投稿一覧
+ *
+ * Class FetchUserPostsUseCase
+ * @package App\UseCases
+ */
+class FetchUserPostsUseCase
+{
+    public function __invoke($userId, $postId, $type)
+    {
+        //
+    }
+}
