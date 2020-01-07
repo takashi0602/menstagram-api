57728fdd4381d8ac96e5a975786a40454b01452a
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-07 20:39:10 +0900

    Fix post liker api

--- app/UseCases/FetchPostLikerUseCase.php
+++ app/UseCases/FetchPostLikerUseCase.php
@@ -2,7 +2,9 @@
 
 namespace App\UseCases;
 
+use App\Models\Follow;
 use App\Models\Like;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * いいねしたユーザー一覧の取得
@@ -18,18 +20,31 @@ class FetchPostLikerUseCase
      */
     public function __invoke($postId)
     {
-        $response = Like::where('post_id', $postId)
-                            ->with(['user'])
-                            ->orderBy('id', 'desc')
-                            ->limit(100)
+        $likes = Like::where('post_id', $postId)
+                        ->with(['user'])
+                        ->orderBy('id', 'desc')
+                        ->limit(100)
+                        ->get();
+
+        $userIds = [];
+        foreach ($likes as $like) {
+            $userIds[] = $like->user->id;
+        }
+
+        $follows = Follow::where('user_id', user()->id)
+                            ->whereIn('target_user_id', $userIds)
                             ->get();
 
-        $response = collect($response)->map(function ($v, $k) {
+        $followIds = collect($follows)->map(function ($v, $k) {
+            return $v->target_user_id;
+        });
+
+        $response = collect($likes)->map(function ($v, $k) use ($followIds) {
             return [
                 'user_id'      => $v['user']['user_id'],
                 'screen_name'  => $v['user']['user_id'],
                 'avatar'       => $v['user']['avatar'],
-                'is_following' => true,
+                'is_following' => collect($followIds)->contains($v['user']['id']) ? true : false,
             ];
         });
 
