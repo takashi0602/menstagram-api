ef679156490c00f818d8f27ab18f8eeea1157c23
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-09 23:52:28 +0900

    Modify user likes api

--- app/UseCases/FetchUserLikesUseCase.php
+++ app/UseCases/FetchUserLikesUseCase.php
@@ -22,7 +22,7 @@ class FetchUserLikesUseCase
     {
         $userId = user()->id;
 
-        $query = Post::with(['user:id,screen_name,avatar']);
+        $query = Post::with(['user:id,user_id,screen_name,avatar']);
 
         if (is_null($postId) && is_null($type))                             $query->latest('id');
         else if (!is_null($postId) && (is_null($type) || $type === 'new'))  $query->where('id', '>=', $postId);
@@ -41,6 +41,10 @@ class FetchUserLikesUseCase
             if (collect($like)->isEmpty()) $isLiked = false;
 
             return collect($v)
+                        ->map(function ($v, $k) {
+                            if ($k === 'user') return collect($v)->except(['id']);
+                            return $v;
+                        })
                         ->put('is_liked', $isLiked)
                         ->except(['user_id']);
         });
