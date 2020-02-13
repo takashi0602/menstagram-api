810da0d996cd9f6336c0ec0b5ce83518ab04073a
Kazuki Tobita <kazukiti201@gmail.com>
2020-02-14 07:24:24 +0900

    Delete unnecessary code

--- app/Http/Requests/AuthRegisterRequest.php
+++ app/Http/Requests/AuthRegisterRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
 
 /**
  * ユーザーの登録
